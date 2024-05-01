5948e73ce2b17d73f90944f019088fb4ae5eb891
phung hoang <lnphunghoang190903@gmail.com>
2024-05-01 16:00:22 +0700

    SinhVien Table Interface and Add Button

--- Lab4/Lab4/Form1.Designer.cs
+++ Lab4/Lab4/Form1.Designer.cs
@@ -32,6 +32,18 @@
             this.dgv_Sach = new System.Windows.Forms.DataGridView();
             this.panel_Sach = new System.Windows.Forms.Panel();
             this.panel_SinhVien = new System.Windows.Forms.Panel();
+            this.txb_DiaChi = new System.Windows.Forms.TextBox();
+            this.txb_SDT = new System.Windows.Forms.TextBox();
+            this.txb_TenSV = new System.Windows.Forms.TextBox();
+            this.txb_MSSV = new System.Windows.Forms.TextBox();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Them = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.dgv_SinhVien = new System.Windows.Forms.DataGridView();
             this.mSSVDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tenSVDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -40,18 +52,6 @@
             this.sINHVIENBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.lab4_QuanLyThuVienDataSet = new Lab4.Lab4_QuanLyThuVienDataSet();
             this.sINHVIENTableAdapter = new Lab4.Lab4_QuanLyThuVienDataSetTableAdapters.SINHVIENTableAdapter();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.btn_Them = new System.Windows.Forms.Button();
-            this.btn_Sua = new System.Windows.Forms.Button();
-            this.btn_Xoa = new System.Windows.Forms.Button();
-            this.txb_MSSV = new System.Windows.Forms.TextBox();
-            this.txb_TenSV = new System.Windows.Forms.TextBox();
-            this.txb_SDT = new System.Windows.Forms.TextBox();
-            this.txb_DiaChi = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Sach)).BeginInit();
             this.panel_Sach.SuspendLayout();
             this.panel_SinhVien.SuspendLayout();
@@ -64,7 +64,7 @@
             // 
             this.dgv_Sach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv_Sach.Location = new System.Drawing.Point(4, 358);
-            this.dgv_Sach.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgv_Sach.Margin = new System.Windows.Forms.Padding(4);
             this.dgv_Sach.Name = "dgv_Sach";
             this.dgv_Sach.RowHeadersWidth = 51;
             this.dgv_Sach.Size = new System.Drawing.Size(861, 392);
@@ -75,7 +75,7 @@
             this.panel_Sach.Controls.Add(this.dgv_Sach);
             this.panel_Sach.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel_Sach.Location = new System.Drawing.Point(16, 15);
-            this.panel_Sach.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel_Sach.Margin = new System.Windows.Forms.Padding(4);
             this.panel_Sach.Name = "panel_Sach";
             this.panel_Sach.Size = new System.Drawing.Size(869, 754);
             this.panel_Sach.TabIndex = 2;
@@ -97,10 +97,113 @@
             this.panel_SinhVien.Controls.Add(this.dgv_SinhVien);
             this.panel_SinhVien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel_SinhVien.Location = new System.Drawing.Point(893, 15);
-            this.panel_SinhVien.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel_SinhVien.Margin = new System.Windows.Forms.Padding(4);
             this.panel_SinhVien.Name = "panel_SinhVien";
             this.panel_SinhVien.Size = new System.Drawing.Size(556, 754);
             this.panel_SinhVien.TabIndex = 3;
+            // 
+            // txb_DiaChi
+            // 
+            this.txb_DiaChi.Location = new System.Drawing.Point(188, 203);
+            this.txb_DiaChi.Name = "txb_DiaChi";
+            this.txb_DiaChi.Size = new System.Drawing.Size(317, 30);
+            this.txb_DiaChi.TabIndex = 12;
+            // 
+            // txb_SDT
+            // 
+            this.txb_SDT.Location = new System.Drawing.Point(188, 160);
+            this.txb_SDT.Name = "txb_SDT";
+            this.txb_SDT.Size = new System.Drawing.Size(317, 30);
+            this.txb_SDT.TabIndex = 11;
+            // 
+            // txb_TenSV
+            // 
+            this.txb_TenSV.Location = new System.Drawing.Point(188, 118);
+            this.txb_TenSV.Name = "txb_TenSV";
+            this.txb_TenSV.Size = new System.Drawing.Size(317, 30);
+            this.txb_TenSV.TabIndex = 10;
+            // 
+            // txb_MSSV
+            // 
+            this.txb_MSSV.Location = new System.Drawing.Point(188, 78);
+            this.txb_MSSV.Name = "txb_MSSV";
+            this.txb_MSSV.Size = new System.Drawing.Size(317, 30);
+            this.txb_MSSV.TabIndex = 9;
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Location = new System.Drawing.Point(406, 287);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(123, 51);
+            this.btn_Xoa.TabIndex = 8;
+            this.btn_Xoa.Text = "Xoá";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Location = new System.Drawing.Point(213, 287);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(123, 51);
+            this.btn_Sua.TabIndex = 7;
+            this.btn_Sua.Text = "Sửa";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Location = new System.Drawing.Point(20, 287);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(123, 51);
+            this.btn_Them.TabIndex = 6;
+            this.btn_Them.Text = "Thêm";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            this.btn_Them.Click += new System.EventHandler(this.btn_Them_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Địa Chỉ:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "SĐT:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tên SV:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MSSV:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(183, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TABLE: SinhVien";
             // 
             // dgv_SinhVien
             // 
@@ -113,7 +216,7 @@
             this.diaChiDataGridViewTextBoxColumn});
             this.dgv_SinhVien.DataSource = this.sINHVIENBindingSource;
             this.dgv_SinhVien.Location = new System.Drawing.Point(4, 358);
-            this.dgv_SinhVien.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgv_SinhVien.Margin = new System.Windows.Forms.Padding(4);
             this.dgv_SinhVien.Name = "dgv_SinhVien";
             this.dgv_SinhVien.RowHeadersWidth = 51;
             this.dgv_SinhVien.Size = new System.Drawing.Size(548, 392);
@@ -165,108 +268,6 @@
             // sINHVIENTableAdapter
             // 
             this.sINHVIENTableAdapter.ClearBeforeFill = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(183, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(212, 29);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "TABLE: SinhVien";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(65, 78);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 25);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "MSSV:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(65, 118);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 25);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Tên SV:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(65, 160);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(59, 25);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "SĐT:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(65, 203);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(82, 25);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Địa Chỉ:";
-            // 
-            // btn_Them
-            // 
-            this.btn_Them.Location = new System.Drawing.Point(20, 287);
-            this.btn_Them.Name = "btn_Them";
-            this.btn_Them.Size = new System.Drawing.Size(123, 51);
-            this.btn_Them.TabIndex = 6;
-            this.btn_Them.Text = "Thêm";
-            this.btn_Them.UseVisualStyleBackColor = true;
-            // 
-            // btn_Sua
-            // 
-            this.btn_Sua.Location = new System.Drawing.Point(213, 287);
-            this.btn_Sua.Name = "btn_Sua";
-            this.btn_Sua.Size = new System.Drawing.Size(123, 51);
-            this.btn_Sua.TabIndex = 7;
-            this.btn_Sua.Text = "Sửa";
-            this.btn_Sua.UseVisualStyleBackColor = true;
-            // 
-            // btn_Xoa
-            // 
-            this.btn_Xoa.Location = new System.Drawing.Point(406, 287);
-            this.btn_Xoa.Name = "btn_Xoa";
-            this.btn_Xoa.Size = new System.Drawing.Size(123, 51);
-            this.btn_Xoa.TabIndex = 8;
-            this.btn_Xoa.Text = "Xoá";
-            this.btn_Xoa.UseVisualStyleBackColor = true;
-            // 
-            // txb_MSSV
-            // 
-            this.txb_MSSV.Location = new System.Drawing.Point(188, 78);
-            this.txb_MSSV.Name = "txb_MSSV";
-            this.txb_MSSV.Size = new System.Drawing.Size(317, 30);
-            this.txb_MSSV.TabIndex = 9;
-            // 
-            // txb_TenSV
-            // 
-            this.txb_TenSV.Location = new System.Drawing.Point(188, 118);
-            this.txb_TenSV.Name = "txb_TenSV";
-            this.txb_TenSV.Size = new System.Drawing.Size(317, 30);
-            this.txb_TenSV.TabIndex = 10;
-            // 
-            // txb_SDT
-            // 
-            this.txb_SDT.Location = new System.Drawing.Point(188, 160);
-            this.txb_SDT.Name = "txb_SDT";
-            this.txb_SDT.Size = new System.Drawing.Size(317, 30);
-            this.txb_SDT.TabIndex = 11;
-            // 
-            // txb_DiaChi
-            // 
-            this.txb_DiaChi.Location = new System.Drawing.Point(188, 203);
-            this.txb_DiaChi.Name = "txb_DiaChi";
-            this.txb_DiaChi.Size = new System.Drawing.Size(317, 30);
-            this.txb_DiaChi.TabIndex = 12;
             // 
             // Form1
             // 
--- Lab4/Lab4/Form1.cs
+++ Lab4/Lab4/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace Lab4
 {
     public partial class Form1 : Form
     {
+        // Remember to change connectionString to YOURS
+        string connectionString = "Data Source=DESKTOP-9188G78\\SQLEXPRESS;Initial Catalog=Lab4_QuanLyThuVien;Integrated Security=True;";
         public Form1()
         {
             InitializeComponent();
@@ -34,6 +37,61 @@ namespace Lab4
                 txb_TenSV.Text = row.Cells[1].Value.ToString();
                 txb_SDT.Text = row.Cells[2].Value.ToString();
                 txb_DiaChi.Text = row.Cells[3].Value.ToString();
+            }
+        }
+
+        private void LoadData()
+        {
+            string query = "SELECT * FROM SINHVIEN";
+
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+                    dataAdapter.Fill(dataTable);
+                }
+            }
+
+            dgv_SinhVien.DataSource = dataTable;
+        }
+
+        private void btn_Them_Click(object sender, EventArgs e)
+        {
+            int mssv = int.Parse(txb_MSSV.Text);
+            string tenSV = txb_TenSV.Text;
+            string sdt = txb_SDT.Text;
+            string diaChi = txb_DiaChi.Text;
+
+            string query = "INSERT INTO SINHVIEN (MSSV, TenSV, SDT, DiaChi) VALUES (@MSSV, @TenSV, @SDT, @DiaChi)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@MSSV", mssv);
+                    command.Parameters.AddWithValue("@TenSV", tenSV);
+                    command.Parameters.AddWithValue("@SDT", sdt);
+                    command.Parameters.AddWithValue("@DiaChi", diaChi);
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Data added successfully.");
+
+                        LoadData();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to add data.");
+                    }
+                }
             }
         }
     }
