ca364f598fef525f5e1be4db03bed3999884ccbe
phung hoang <lnphunghoang190903@gmail.com>
2024-05-01 14:54:30 +0700

    Modify Query to run at once

--- Lab4.sql
+++ Lab4.sql
@@ -1,6 +1,9 @@
-﻿CREATE DATABASE Lab4_QuanLyThuVien
+﻿CREATE DATABASE Lab4_QuanLyThuVien2;
+GO
 
-USE Lab4_QuanLyThuVien
+USE Lab4_QuanLyThuVien2;
+GO
+
 -- Tạo bảng cho thực thể SinhVien
 CREATE TABLE SINHVIEN (
     MSSV INT PRIMARY KEY,
@@ -20,7 +23,6 @@ CREATE TABLE SACH (
     MSSV int FOREIGN KEY REFERENCES SinhVien(MSSV)
 );
 
-USE Lab4_QuanLyThuVien
 -- Thêm dữ liệu vào bảng SinhVien
 INSERT INTO SinhVien (MSSV, TenSV, SDT, DiaChi) VALUES 
 (101, N'Lê Nguyễn Phụng Hoàng', '0123456789', N'Số 10, Đường Pasteur, Quận 1, TP. HCM'),
@@ -29,7 +31,6 @@ INSERT INTO SinhVien (MSSV, TenSV, SDT, DiaChi) VALUES
 (104, N'Nguyễn An', '0901234567', N'Số 40, Đường Pasteur, Quận 4, TP. HCM'),
 (105, N'Trần Cường', '0365478912', N'Số 50, Đường Cô Giang, Quận 5, TP. HCM');
 
-USE Lab4_QuanLyThuVien
 -- Thêm dữ liệu vào bảng Sach
 INSERT INTO Sach (TenSach, NXB, TG, TheLoai, MoTa) VALUES 
 (N'Cuốn theo chiều gió', N'NXB Kim Đồng', N'Margaret Mitchell', N'Tiểu thuyết lịch sử', N'Cuốn sách nổi tiếng về cuộc đời và tình yêu của Scarlett O''Hara và Rhett Butler.'),
@@ -37,6 +38,4 @@ INSERT INTO Sach (TenSach, NXB, TG, TheLoai, MoTa) VALUES
 (N'Nhà Giả Kim', N'NXB Hội Nhà Văn', 'Paulo Coelho', N'Tiểu thuyết hành động', N'Cuốn sách kể về hành trình tìm kiếm đích thực của một nhà Giả Kim.'),
 (N'Sử ta diễn ra như thế nào', N'NXB Đại Học Quốc Gia Hà Nội', N'Nguyễn Phú Trọng', N'Báo cáo chính trị', N'Sách tập hợp các diễn văn, bài phát biểu của Chủ tịch nước Nguyễn Phú Trọng.'),
 (N'Toán học vui', N'NXB Giáo Dục', N'Nguyễn Văn Bình', N'Sách giáo khoa', N'Cuốn sách giúp học sinh tiểu học yêu thích môn Toán hơn.');
-
-
-
+GO
